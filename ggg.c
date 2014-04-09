f7bc308b010b2d8e312cf563bbb6772f5d790431
Bozo Dragojevic <bozzo@digiverse.si>
2014-04-10 00:39:04 +0200

    Catch up with changed proton's event interface

--- ggg.c
+++ ggg.c
@@ -13,27 +13,27 @@
 #include "lp.h"
 
 void process_connection(ldp_connection_t *conn, pn_event_t *event) {
-    fprintf(stderr, "connection event\n");
+    fprintf(stderr, "connection event %s\n", pn_event_type_name(pn_event_type(event)));
 }
 
 void process_session(ldp_connection_t *conn, pn_event_t *event) {
-    fprintf(stderr, "session event\n");
+    fprintf(stderr, "session event %s\n", pn_event_type_name(pn_event_type(event)));
 }
 
 void process_link(ldp_connection_t *conn, pn_event_t *event) {
-    fprintf(stderr, "link event\n");
+    fprintf(stderr, "link event %s\n", pn_event_type_name(pn_event_type(event)));
 }
 
 void process_flow(ldp_connection_t *conn, pn_event_t *event) {
-    fprintf(stderr, "flow event\n");
+    fprintf(stderr, "flow event %s\n", pn_event_type_name(pn_event_type(event)));
 }
 
 void process_delivery(ldp_connection_t *conn, pn_event_t *event) {
-    fprintf(stderr, "delivery event\n");
+    fprintf(stderr, "delivery event %s\n", pn_event_type_name(pn_event_type(event)));
 }
 
 void process_transport(ldp_connection_t *conn, pn_event_t *event) {
-    fprintf(stderr, "transport event\n");
+    fprintf(stderr, "transport event %s\n", pn_event_type_name(pn_event_type(event)));
 }
 
 void events(ldp_connection_t *conn, pn_collector_t *coll) {
@@ -42,13 +42,16 @@ void events(ldp_connection_t *conn, pn_collector_t *coll) {
         switch (pn_event_type(event)) {
         case PN_EVENT_NONE:
             break;
-        case PN_CONNECTION_STATE:
+        case PN_CONNECTION_REMOTE_STATE:
+        case PN_CONNECTION_LOCAL_STATE:
             process_connection(conn, event);
             break;
-        case PN_SESSION_STATE:
+        case PN_SESSION_REMOTE_STATE:
+        case PN_SESSION_LOCAL_STATE:
             process_session(conn, event);
             break;
-        case PN_LINK_STATE:
+        case PN_LINK_REMOTE_STATE:
+        case PN_LINK_LOCAL_STATE:
             process_link(conn, event);
             break;
         case PN_LINK_FLOW:
@@ -71,7 +74,7 @@ int main() {
     WSADATA wsadata;
     WSAStartup(MAKEWORD(2, 2), &wsadata);
 #endif
-	ldp_connection_t *conn = ldp_connection(events);
-	ldp_connection_connect(conn, "host", "8194");
+    ldp_connection_t *conn = ldp_connection(events);
+    ldp_connection_connect(conn, "host", "8194");
     dispatch_main();
 }
